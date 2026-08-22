280aed6199cb74d788bb2914d2c3a738385de02d
Atharva Ghule <atharvachamp45@gmail.com>
2026-08-22 21:01:12 +0530

    fix: remove upward shift for preceding menu options

--- src/App.jsx
+++ src/App.jsx
@@ -275,7 +275,7 @@ function App() {
             containerY = option.baseY;
           } else if (index < activeIndex) {
             containerX = option.baseX;
-            containerY = option.baseY - 4;
+            containerY = option.baseY;
           } else {
             containerX = option.baseX;
             containerY = option.baseY + 130;
